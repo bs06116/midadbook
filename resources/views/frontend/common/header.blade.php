14bda3dc35b604ef9a9a9f813b48587e01191e77
Muhammad Raza <nvtech2020@gmail.com>
2021-08-03 18:04:48 +0500

    update header filer

---
+++ resources/views/frontend/common/header.blade.php
@@ -0,0 +1,97 @@
+<!-- <div class="container-fluid ">
+    <div class="row py-2 d-flex text-center">
+        <div class=" col-3 col-xs-3 col-sm-3 col-md-4 col-lg-4 col-xl-4 ">
+
+    <nav class="px-1 navbar navbar-expand-lg navbar-light  w-100">
+        <div class="container-fluid">
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse   "
+                id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item px-4 border rounded-pill mr-2 ">
+                        <a class="nav-link active text-light" aria-current="page" href="{{route('user/login')}}">  {{__('translation.login')}}
+
+                        </a>
+                    </li>
+                    <li class="nav-item px-4 border rounded-pill">
+                        <a class="nav-link text-light" href="{{route('user/login')}}">  {{__('translation.signup')}}
+
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    </nav>
+    </div>
+    <div class="  col-4 col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 text-center logo ">
+        <a class="px-4 navbar-brand text-light" href="#"><img src="{{asset('assets/img/front/midad_ul_maktaba_logo_header.png') }}" alt=""></a>
+    </div>
+    <div class="pt-1 col-5 col-xs-5 col-sm-5 col-md-4 col-lg-4 col-xl-4  btn-div">
+        <button class="d-flex mx-auto    nav-btn rounded-pill pull-right  mt-2 text-light "><span class="px-4 ">Hello</span> <i
+                class="py-2 pull-right  fa fa-plus" aria-hidden="true"></i></button>
+    </div>
+    </div>
+    
+</div> -->
+<div class="container-fluid ">
+<div class="row py-2 d-flex ">
+<div class="col-4   ">
+    <div class="row d-flex">
+        <div class="col-6 login-nav d-flex">
+        <nav class="navbar navbar-expand-lg navbar-light users-img py-2 ">
+          
+            
+          <div class="user-profile-img ">
+          <img src="{{asset('assets/img/front/profile_thumbnail1.png') }}" alt="">
+          </div>
+          <div class="user-name login-user1 text-light ">
+            <span class="d-block font-weight-bold">Name</span>
+            <span>countery</span>
+          </div>
+          </nav>
+        </div>
+    
+    <div class="col-6 px-4 pt-3 text-center  login-nav login-nav1">
+    <div class="notifaction-icon d-flex login-nav2">
+              <div class="noti"><img src="{{asset('assets/img/front/bell_ic.png') }}" alt=""></div>
+                <div class="noti"><img src="{{asset('assets/img/front/bell_ic.png') }}" alt="">
+                  <i class="badge1">9</i></div>
+                <div class=""><img src="{{asset('assets/img/front/send_ic.png') }}" alt=""> 
+                  <i class="badge1">9</i></div>
+              </div>
+    </div>
+    </div>
+          </div>
+            
+            <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse col-4 ml-4" id="navbarSupportedContent">
+               <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item px-2 border rounded-pill mr-2 ">
+                  <a class="nav-link active text-light" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item px-2 border rounded-pill">
+                  <a class="nav-link text-light" href="#">About</a>
+                </li>
+              </ul>
+            </div> -->
+            <div class="col-3 col-xs-3 col-sm-3 col-md-4 col-lg-4 col-xl-4 text-center logo   ">
+                <a class=" px-4 navbar-brand text-light" href="#"><img src="{{asset('assets/img/front/midad_ul_maktaba_logo_header.png') }}" alt=""></a>
+              </div>
+            <!-- <div class="col-4 col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 text-right btn1-div">
+            <button class="  border nav1-btn rounded-pill pull-right  mt-3 text-light "><span class="">Hello</span> <i class=" pull-right  fa fa-plus plus-icon" aria-hidden="true"></i></button>
+          </div> -->
+          <div class=" col-5 col-xs-5 col-sm-5 col-md-4 col-lg-4 col-xl-4 text-left btn1-div  ">
+        <button class="d-flex   nav-btn nav-btnn rounded-pill pull-right  mt-3 text-light "><span class="pt-1 px-3 ">Hello</span> <i
+                class="py-2 pull-right  fa fa-plus" aria-hidden="true"></i></button>
+    </div>
+          
+</div>
+        </div>

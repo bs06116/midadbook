4780cb672232df4acf85e15189b6da6b367d5bab
Muhammad Raza <nvtech2020@gmail.com>
2021-08-06 13:10:48 +0500

    notification dropdown update

--- resources/views/frontend/common/header_register.blade.php
+++ resources/views/frontend/common/header_register.blade.php
@@ -1,36 +1,157 @@
 
 <div class="container-fluid ">
     <div class="row py-2 d-flex ">
-        <div class="col-4   ">
-            <div class="row d-flex">
-                <div class="col-6 login-nav d-flex">
+        <div class="col-4  d-flex nav2">
+            
                     <nav class="navbar navbar-expand-lg navbar-light users-img py-2">
                         <div class="user-profile-img ">
-                            <img src="{{ asset('assets/img/front/profile_thumbnail1.png') }}" alt="">
+                            <img id="user-icon" src="{{ asset('assets/img/front/profile_thumbnail_large.png') }}" alt="">
                         </div>
-                        <div class="user-name login-user1 text-light d-bloak ">
+                        <div class="user-name login-user1 text-light ">
                             <span class="d-block font-weight-bold">{{Auth::user()->name}}</span>
                             <span>{{Auth::user()->city->name}}</span>
                         </div>
                     </nav>
-                </div>
+                
 
-                <div class="col-6 px-4 pt-3 text-center  login-nav login-nav1">
-                    <div class="notifaction-icon d-flex login-nav2">
-                        <div class="noti"><img src="{{ asset('assets/img/front/bell_ic.png') }}" alt=""></div>
-                        <div class="noti"><img src="{{ asset('assets/img/front/bell_ic.png') }}" alt="">
+                
+                    <div class="notifaction-icon  login-nav2">
+                        <div class="noti mt-4"><img src="{{ asset('assets/img/front/bell_ic.png') }}" alt=""></div>
+                        <div class="noti mt-4" id="bell-icon"><img src="{{ asset('assets/img/front/bell_ic.png') }}" alt="">
                             <i class="badge1">9</i>
                         </div>
-                        
-                        <div class=""><img src="{{ asset('assets/img/front/send_ic.png') }}" alt="">
+                        <div class="mt-4"><img src="{{ asset('assets/img/front/send_ic.png') }}" alt="">
                             <i class="badge1">9</i>
                         </div>
                     </div>
-                </div>
-            </div>
+               
         </div>
-
-
+        <div class="user-menu toggle text-right" id="toggle">
+           <h4><a href="#"></a> Login</h4>
+           <h4><a href="#"></a> Logout</h4>
+              </div>
+        <div class="notification_dd toggel" id="toggel">
+            <ul class="notification_ul">
+                <li class="starbucks success">
+                    <div class="notify_icon">
+                    <img src="{{ asset('assets/img/front/profile_thumbnail1.png') }}" alt="">  
+                    </div>
+                    <div class="notify_data">
+                        <div class="title">
+                            Lorem, ipsum dolor.  
+                        </div>
+                        <div class="sub_title">
+                          Lorem ipsum dolor sit amet consectetur.
+                      </div>
+                    </div>
+                    <div class="notify_status">
+                        <p>Success</p>  
+                    </div>
+                </li>
+                <hr>
+                <li class="starbucks success">
+                    <div class="notify_icon">
+                    <img src="{{ asset('assets/img/front/profile_thumbnail1.png') }}" alt="">  
+                    </div>
+                    <div class="notify_data">
+                        <div class="title">
+                            Lorem, ipsum dolor.  
+                        </div>
+                        <div class="sub_title">
+                          Lorem ipsum dolor sit amet consectetur.
+                      </div>
+                    </div>
+                    <div class="notify_status">
+                        <p>Success</p>  
+                    </div>
+                </li>
+                <hr>
+                <li class="starbucks success">
+                    <div class="notify_icon">
+                    <img src="{{ asset('assets/img/front/profile_thumbnail1.png') }}" alt="">  
+                    </div>
+                    <div class="notify_data">
+                        <div class="title">
+                            Lorem, ipsum dolor.  
+                        </div>
+                        <div class="sub_title">
+                          Lorem ipsum dolor sit amet consectetur.
+                      </div>
+                    </div>
+                    <div class="notify_status">
+                        <p>Success</p>  
+                    </div>
+                </li>
+                <hr>
+                <li class="starbucks success">
+                    <div class="notify_icon">
+                    <img src="{{ asset('assets/img/front/profile_thumbnail1.png') }}" alt="">  
+                    </div>
+                    <div class="notify_data">
+                        <div class="title">
+                            Lorem, ipsum dolor.  
+                        </div>
+                        <div class="sub_title">
+                          Lorem ipsum dolor sit amet consectetur.
+                      </div>
+                    </div>
+                    <div class="notify_status">
+                        <p>Success</p>  
+                    </div>
+                </li>
+                <hr>
+                <li class="starbucks success">
+                    <div class="notify_icon">
+                    <img src="{{ asset('assets/img/front/profile_thumbnail1.png') }}" alt="">  
+                    </div>
+                    <div class="notify_data">
+                        <div class="title">
+                            Lorem, ipsum dolor.  
+                        </div>
+                        <div class="sub_title">
+                          Lorem ipsum dolor sit amet consectetur.
+                      </div>
+                    </div>
+                    <div class="notify_status">
+                        <p>Success</p>  
+                    </div>
+                </li>
+                <hr>
+                <li class="starbucks success">
+                    <div class="notify_icon">
+                    <img src="{{ asset('assets/img/front/profile_thumbnail1.png') }}" alt="">  
+                    </div>
+                    <div class="notify_data">
+                        <div class="title">
+                            Lorem, ipsum dolor.  
+                        </div>
+                        <div class="sub_title">
+                          Lorem ipsum dolor sit amet consectetur.
+                      </div>
+                    </div>
+                    <div class="notify_status">
+                        <p>Success</p>  
+                    </div>
+                </li>
+                <hr>
+                <li class="starbucks success">
+                    <div class="notify_icon">
+                    <img src="{{ asset('assets/img/front/profile_thumbnail1.png') }}" alt="">  
+                    </div>
+                    <div class="notify_data">
+                        <div class="title">
+                            Lorem, ipsum dolor.  
+                        </div>
+                        <div class="sub_title">
+                          Lorem ipsum dolor sit amet consectetur.
+                      </div>
+                    </div>
+                    <div class="notify_status">
+                        <p>Success</p>  
+                    </div>
+                </li>
+                </ul>
+              </div>
         <div class="col-3 col-xs-3 col-sm-3 col-md-4 col-lg-4 col-xl-4 text-center logo   ">
             <a class=" px-4 navbar-brand text-light" href="#"><img
                     src="{{ asset('assets/img/front/midad_ul_maktaba_logo_header.png') }}" alt=""></a>
@@ -42,8 +163,55 @@
                         class="pt-1 px-3 "> {{ __('translation.add_book') }}</span> <i class="py-2 pull-right  fa fa-plus"
                         aria-hidden="true"></i></button>
                     </a>
+            
         </div>
 
-
+        
     </div>
+    
 </div>
+
+
+
+
+
+
+<script>
+
+var bell = document.getElementById("bell-icon");
+ 
+var toggel = document.getElementById("toggel");
+
+bell.addEventListener("click", dropdown);
+
+function dropdown(){
+    
+    if(!toggel.classList.value.includes('active')){
+    toggel.classList.add('active');
+    }
+    else{
+        toggel.classList.remove('active');
+    }
+    
+}
+
+
+var user = document.getElementById("user-icon");
+ 
+var toggle = document.getElementById("toggle");
+
+user.addEventListener("click", dropdown1);
+
+function dropdown1(){
+    
+    if(!toggle.classList.value.includes('active')){
+    toggle.classList.add('active');
+    }
+    else{
+        toggle.classList.remove('active');
+    }
+    
+}
+
+</script>
+
